989e0989e41d582963acf2e0486c419205054861
Florian Krug <florian.krug@zeiss.com>
2025-10-21 14:04:25 +0200

    implemented missing key functions

--- BlyatOS/Kernel.cs
+++ BlyatOS/Kernel.cs
@@ -1,9 +1,10 @@
-using BadTetrisCS;
+﻿using BadTetrisCS;
 using BlyatOS.Library.Configs;
 using BlyatOS.Library.Functions;
 using BlyatOS.Library.Startupthings;
 using Cosmos.HAL;
 using System;
+using System.IO;
 using Cosmos.System.ScanMaps;
 using Sys = Cosmos.System;
 using BlyatOS.Library.BlyatFileSystem;
@@ -21,7 +22,9 @@ public class Kernel : Sys.Kernel
 
     Sys.FileSystem.CosmosVFS fs;
     FileSystemHelpers fsh = new FileSystemHelpers();
-    string current_directory = "0:\\";
+
+    private const string RootPath = @"0:\";
+    private string current_directory = RootPath; // immer mit abschließendem Backslash
 
     protected override void BeforeRun()
     {
@@ -29,7 +32,6 @@ public class Kernel : Sys.Kernel
         Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
 
         Sys.KeyboardManager.SetKeyLayout(new DE_Standard());
-        //OnStartUp.RunLoadingScreenThing();
         Console.WriteLine($"BlyatOS v{versionString} booted successfully. Type help for a list of valid commands");
         momentOfStart = DateTime.Now;
         Global.PIT.Wait(1000);
@@ -39,182 +41,237 @@ public class Kernel : Sys.Kernel
     {
         try
         {
+            // Verzeichnislisten immer aktuell holen
             string[] dirs = fsh.GetDirectories(current_directory);
+            string[] files = fsh.GetFiles(current_directory);
+
             if (logged_in)
             {
                 Console.Write("Input: ");
                 var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    return;
 
-                string[] args = input.Split(' ');
-
+                string[] args = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 switch (args[0])
                 {
                     case "help":
                         {
                             int? page = null;
-                            if (args.Length > 1)
-                            {
-                                if (int.TryParse(args[1], out int p)) page = p;
-                            }
+                            if (args.Length > 1 && int.TryParse(args[1], out int p)) page = p;
                             Console.Clear();
                             BasicFunctions.Help(page);
                             break;
                         }
-                    case "throwTestException1":
-                        {
-                            throw new GenericException("This is a 1st test exception");
-
-                        }
-                    case "throwTestException2":
-                        {
-                            throw new GenericException("This is a 2nd test exception", "TEST_EXCEPTION 2");
-                        }
-                    case "throwTestException3":
-                        {
-                            throw new GenericException("This is a 3rd test exception", "TEST_EXCEPTION 3", "from this command wuwuwu");
-                        }
                     case "version":
                     case "v":
-                        {
-                            Console.WriteLine("Blyat version " + versionString);
-                            break;
-                        }
+                        Console.WriteLine("Blyat version " + versionString);
+                        break;
+
                     case "echo":
-                        {
-                            BasicFunctions.EchoFunction(args);
-                            break;
-                        }
+                        BasicFunctions.EchoFunction(args);
+                        break;
+
                     case "runtime":
-                        {
-                            Console.WriteLine(BasicFunctions.RunTime(momentOfStart));
-                            break;
-                        }
+                        Console.WriteLine(BasicFunctions.RunTime(momentOfStart));
+                        break;
+
                     case "wiseman":
-                        {
-                            Console.WriteLine(BasicFunctions.GenerateWiseManMessage(Rand));
-                            break;
-                        }
+                        Console.WriteLine(BasicFunctions.GenerateWiseManMessage(Rand));
+                        break;
+
                     case "reboot":
-                        {
-                            Console.WriteLine("rebooting");
-                            Global.PIT.Wait(1000);
-                            Cosmos.System.Power.Reboot();
-                            break;
-                        }
+                        Console.WriteLine("rebooting");
+                        Global.PIT.Wait(1000);
+                        Cosmos.System.Power.Reboot();
+                        break;
+
                     case "exit":
-                        {
-                            Console.WriteLine("exitting");
-                            Global.PIT.Wait(1000);
-                            Cosmos.System.Power.Shutdown();
-                            break;
-                        }
+                        Console.WriteLine("exitting");
+                        Global.PIT.Wait(1000);
+                        Cosmos.System.Power.Shutdown();
+                        break;
+
                     case "cls":
                     case "clear":
                     case "clearScreen":
-                        {
-                            Console.Clear();
-                            break;
-                        }
+                        Console.Clear();
+                        break;
+
                     case "logout":
                     case "lock":
-                        {
-                            Console.WriteLine("Logging out...");
-                            Global.PIT.Wait(1000);
-                            logged_in = false;
-                            break;
-                        }
-                    case "vodka":
-                        {
-                            if (!UserManagement.CheckPermissions(CurrentUser, UsersConf, UsersConfig.Permissions.Admin))
-                            {
-                                Console.WriteLine("Missing Permissions");
-                                break;
-                            }
-                            Console.WriteLine("Nyet, no vodka for you! //not implemented");
-                            break;
-                        }
+                        Console.WriteLine("Logging out...");
+                        Global.PIT.Wait(1000);
+                        logged_in = false;
+                        break;
+
                     case "createUser":
+                        Console.Clear();
+                        if (!UserManagement.CheckPermissions(CurrentUser, UsersConf, UsersConfig.Permissions.Admin))
                         {
-                            Console.Clear();
-                            if (!UserManagement.CheckPermissions(CurrentUser, UsersConf, UsersConfig.Permissions.Admin))
-                            {
-                                Console.WriteLine("Missing Permissions");
-                                break;
-                            }
-                            UserManagement.CreateUser(UsersConf, CurrentUser);
+                            Console.WriteLine("Missing Permissions");
                             break;
                         }
+                        UserManagement.CreateUser(UsersConf, CurrentUser);
+                        break;
+
                     case "deleteUser":
+                        Console.Clear();
+                        if (!UserManagement.CheckPermissions(CurrentUser, UsersConf, UsersConfig.Permissions.Admin))
                         {
-                            Console.Clear();
-                            if (!UserManagement.CheckPermissions(CurrentUser, UsersConf, UsersConfig.Permissions.Admin))
+                            Console.WriteLine("Missing Permissions");
+                            break;
+                        }
+                        UserManagement.DeleteUser(UsersConf, CurrentUser);
+                        break;
+
+                    case "tetris":
+                        new BadTetris().Run();
+                        break;
+
+                    case "fsinfo":
+                        {
+                            var disks = fs.Disks;
+                            foreach (var disk in disks)
                             {
-                                Console.WriteLine("Missing Permissions");
+                                if (disk?.Host == null)
+                                {
+                                    Console.WriteLine("Disk host unavailable");
+                                    continue;
+                                }
+
+                                Console.WriteLine(fsh.BlockDeviceTypeToString(disk.Host.Type));
+
+                                foreach (var partition in disk.Partitions)
+                                {
+                                    Console.WriteLine(partition.Host);
+                                    Console.WriteLine(partition.RootPath);
+                                    Console.WriteLine(partition.MountedFS.Size);
+                                }
+                            }
+                            break;
+                        }
+
+                    case "dir":
+                        {
+                            foreach (var d in dirs)
+                            {
+                                Console.WriteLine(TrimPath(d) + "/");
+                            }
+                            break;
+                        }
+
+                    case "ls":
+                        {
+                            const string dirMarker = "[D]";
+                            const string fileMarker = "[F]";
+                            foreach (var d in dirs)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.WriteLine($"{dirMarker} {TrimPath(d)}/");
+                            }
+                            foreach (var f in files)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                Console.WriteLine($"{fileMarker} {TrimPath(f)}");
+                            }
+                            Console.ResetColor();
+                            break;
+                        }
+
+                    case "mkdir":
+                        {
+                            if (args.Length < 2)
+                            {
+                                Console.WriteLine("Usage: mkdir <name>");
                                 break;
                             }
-                            UserManagement.DeleteUser(UsersConf, CurrentUser);
+                            var name = args[1];
+                            var path = PathCombine(current_directory, name);
+                            fs.CreateDirectory(path);
+                            Console.WriteLine($"Directory '{name}' created at '{path}'");
                             break;
                         }
-                    case "tetris":
-                        {
-                            BadTetris game = new BadTetris();
-                            game.Run();
-                            break;
-                        }
-                    case "fsinfo":
-                    {
-                        var disks = fs.Disks;
-                        foreach (var disk in disks)
-                        {
-                            if (disk?.Host == null)
-                            {
-                                Console.WriteLine("Disk host unavailable");
-                                continue;
-                            }
 
-                            Console.WriteLine(fsh.BlockDeviceTypeToString(disk.Host.Type));
-
-                            foreach (var partition in disk.Partitions)
-                            {
-                                Console.WriteLine(partition.Host);
-                                Console.WriteLine(partition.RootPath);
-                                Console.WriteLine(partition.MountedFS.Size);
-                            }
-                        }
-                        break;
-                    }
-                    case "dir":
-                    {
-                        foreach (var item in dirs)
-                        {
-                            Console.WriteLine(item);
-                        }
-
-                        break;
-                    }
-                    case "mkdir":
-                    {
-                        string? name = null;
-                        if (args.Length > 1) name = args[1];
-                        var path = current_directory + name;
-                        fs.CreateDirectory(path);
-                        Console.WriteLine($"Directory '{name}' created at '{path}'");
-                        break;
-                    }
                     case "touch":
-                    {
-                        string? name = null;
-                        if (args.Length > 1) name = args[1];
-                        var path = current_directory + name;
-                        fs.CreateFile(path);
-                        Console.WriteLine($"File '{name}' created at '{path}'");
-                        break;
-                    }
-                    default:
                         {
-                            Console.WriteLine("Unknown command! Enter \"help\" for more information!");
+                            if (args.Length < 2)
+                            {
+                                Console.WriteLine("Usage: touch <name>");
+                                break;
+                            }
+                            var name = args[1];
+                            var path = PathCombine(current_directory, name);
+                            fs.CreateFile(path);
+                            Console.WriteLine($"File '{name}' created at '{path}'");
                             break;
                         }
+
+                    case "cat":
+                        {
+                            if (args.Length < 2)
+                            {
+                                Console.WriteLine("Usage: cat <filename>");
+                                break;
+                            }
+                            var fileArg = args[1];
+                            string path = IsAbsolute(fileArg) ? fileArg : PathCombine(current_directory, fileArg).TrimEnd('\\');
+
+                            if (!fsh.FileExists(path))
+                            {
+                                Console.WriteLine($"File not found: {path}");
+                                break;
+                            }
+
+                            try
+                            {
+                                Console.WriteLine(fsh.ReadAllText(path));
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Error reading file: " + ex.Message);
+                            }
+                            break;
+                        }
+
+                    case "cd":
+                        {
+                            if (args.Length < 2)
+                            {
+                                Console.WriteLine("Usage: cd <directory>|..");
+                                break;
+                            }
+                            var target = args[1];
+                            if (target == "..")
+                            {
+                                if (IsRoot(current_directory))
+                                {
+                                    Console.WriteLine("Already at root");
+                                    break;
+                                }
+                                current_directory = GetParent(current_directory);
+                                Console.WriteLine($"Changed directory to '{current_directory}'");
+                                break;
+                            }
+
+                            string newPath = IsAbsolute(target) ? EnsureTrailingSlash(target) : PathCombine(current_directory, target);
+                            if (!fsh.DirectoryExists(newPath))
+                            {
+                                Console.WriteLine($"Directory not found: {newPath}");
+                                break;
+                            }
+                            current_directory = EnsureTrailingSlash(newPath);
+                            Console.WriteLine($"Changed directory to '{current_directory}'");
+                            break;
+                        }
+
+                    case "pwd":
+                        Console.WriteLine(current_directory);
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown command! Enter \"help\" for more information!");
+                        break;
                 }
             }
             else
@@ -248,5 +305,48 @@ public class Kernel : Sys.Kernel
         {
             Console.WriteLine("An error occured: " + ex.Message);
         }
+    }
+
+    // Helpers
+
+    private static string TrimPath(string full)
+    {
+        if (string.IsNullOrEmpty(full)) return full;
+        var sep = full.TrimEnd('\\').LastIndexOf('\\');
+        if (sep >= 0 && sep < full.Length - 1)
+            return full.TrimEnd('\\').Substring(sep + 1);
+        return full.TrimEnd('\\');
+    }
+
+    private static bool IsAbsolute(string path) => path.Contains(":\\");
+    private static bool IsRoot(string path) => string.Equals(path, RootPath, StringComparison.Ordinal);
+
+    private static string EnsureTrailingSlash(string path)
+    {
+        if (!path.EndsWith("\\"))
+            return path + "\\";
+        return path;
+    }
+
+    private static string PathCombine(string baseDir, string child)
+    {
+        // Basis immer mit Backslash
+        baseDir = EnsureTrailingSlash(baseDir);
+        // Path.Combine entfernt doppelte Backslashes korrekt
+        var combined = Path.Combine(baseDir, child);
+        return EnsureTrailingSlash(combined);
+    }
+
+    private static string GetParent(string path)
+    {
+        path = EnsureTrailingSlash(path);
+        if (IsRoot(path)) return path;
+        // entfernt letzten Segment-Backslash
+        var trimmed = path.TrimEnd('\\');
+        var idx = trimmed.LastIndexOf('\\');
+        if (idx <= 2) // z.B. "0:\" -> Index 2
+            return RootPath;
+        var parent = trimmed.Substring(0, idx + 1);
+        return EnsureTrailingSlash(parent);
     }
 }
