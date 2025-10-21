587c84d9ec0c956fb6b9f39385ee724b62a1f6d3
Florian Krug <florian.krug@zeiss.com>
2025-10-21 12:08:47 +0200

    basic filesystem with dir, touch

--- BlyatOS/Kernel.cs
+++ BlyatOS/Kernel.cs
@@ -1,16 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Cosmos.HAL;
-using Sys = Cosmos.System;
-using BlyatOS.Library.Startupthings;
-using BlyatOS.Library.Functions;
 using BadTetrisCS;
 using BlyatOS.Library.Configs;
-using Microsoft.VisualBasic.FileIO;
-using System.Linq;
-using System.ComponentModel.Design;
-using BlyatOS.Library;
+using BlyatOS.Library.Functions;
+using BlyatOS.Library.Startupthings;
+using Cosmos.HAL;
+using Cosmos.HAL.BlockDevice;
+using System;
+using System.IO;
+using System.Security.Principal;
+using Cosmos.System.ScanMaps;
+using Sys = Cosmos.System;
+using SysFS = Cosmos.System.FileSystem;
+using SysVFS = Cosmos.System.FileSystem.VFS;
+using BlyatOS.Library.BlyatFileSystem;
 
 namespace BlyatOS;
 
@@ -21,11 +22,19 @@ public class Kernel : Sys.Kernel
     private UsersConfig UsersConf = new UsersConfig();
     private int CurrentUser;
     bool logged_in = false;
-    Random Rand = new Random(DateTime.Now.Millisecond); //universal random so it doesnt need to be set all the time(for functions that need it)
+    Random Rand = new Random(DateTime.Now.Millisecond);
+
+    Sys.FileSystem.CosmosVFS fs;
+    FileSystemHelpers fsh = new FileSystemHelpers();
+    string current_directory = "0:\\";
+
     protected override void BeforeRun()
     {
-        OnStartUp.RunLoadingScreenThing(); //could be removed, but it is cool
-        //VGACursorFix.EnableDebug();
+        fs = new Sys.FileSystem.CosmosVFS();
+        Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+
+        Sys.KeyboardManager.SetKeyLayout(new DE_Standard());
+        //OnStartUp.RunLoadingScreenThing();
         Console.WriteLine($"BlyatOS v{versionString} booted successfully. Type help for a list of valid commands");
         momentOfStart = DateTime.Now;
         Global.PIT.Wait(1000);
@@ -33,11 +42,11 @@ public class Kernel : Sys.Kernel
 
     protected override void Run()
     {
+        string[] dirs = fsh.GetDirectories(current_directory);
         if (logged_in)
         {
             Console.Write("Input: ");
             var input = Console.ReadLine();
-
             string[] args = input.Split(' ');
 
             switch (args[0])
@@ -45,10 +54,7 @@ public class Kernel : Sys.Kernel
                 case "help":
                     {
                         int? page = null;
-                        if (args.Length > 1)
-                        {
-                            if (int.TryParse(args[1], out int p)) page = p;
-                        }
+                        if (args.Length > 1 && int.TryParse(args[1], out int p)) page = p;
                         Console.Clear();
                         BasicFunctions.Help(page);
                         break;
@@ -139,6 +145,55 @@ public class Kernel : Sys.Kernel
                     {
                         BadTetris game = new BadTetris();
                         game.Run();
+                        break;
+                    }
+                case "fsinfo":
+                    {
+                        var disks = fs.Disks;
+                        foreach (var disk in disks)
+                        {
+                            if (disk?.Host == null)
+                            {
+                                Console.WriteLine("Disk host unavailable");
+                                continue;
+                            }
+
+                            Console.WriteLine(fsh.BlockDeviceTypeToString(disk.Host.Type));
+
+                            foreach (var partition in disk.Partitions)
+                            {
+                                Console.WriteLine(partition.Host);
+                                Console.WriteLine(partition.RootPath);
+                                Console.WriteLine(partition.MountedFS.Size);
+                            }
+                        }
+                        break;
+                    }
+                case "dir":
+                    {
+                        foreach (var item in dirs)
+                        {
+                            Console.WriteLine(item);
+                        }
+
+                        break;
+                    }
+                case "mkdir":
+                    {
+                        string? name = null;
+                        if (args.Length > 1) name = args[1];
+                        var path = current_directory + name;
+                        fs.CreateDirectory(path);
+                        Console.WriteLine($"Directory '{name}' created at '{path}'");
+                        break;
+                    }
+                case "touch":
+                    {
+                        string? name = null;
+                        if (args.Length > 1) name = args[1];
+                        var path = current_directory + name;
+                        fs.CreateFile(path);
+                        Console.WriteLine($"File '{name}' created at '{path}'");
                         break;
                     }
                 default:
---
+++ BlyatOS/Library/BlyatFileSystem/FileSystemHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Cosmos.HAL.BlockDevice;
+using System.IO;
+
+namespace BlyatOS.Library.BlyatFileSystem;
+
+public class FileSystemHelpers
+{
+    public  string[] GetDirectories(string path)
+    {
+        return Directory.GetDirectories(path);
+    }
+
+    public  string BlockDeviceTypeToString(BlockDeviceType type) =>
+        type switch
+        {
+            BlockDeviceType.HardDrive => "Hard Drive",
+            BlockDeviceType.Removable => "Removable Drive",
+            BlockDeviceType.RemovableCD => "Removable CD Drive",
+            _ => type.ToString()
+        };
+}
